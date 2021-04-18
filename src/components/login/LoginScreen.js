a30ed793fd59abc08deb8ef90628974e5008017a
Cristian Guaman <cristian.guaman@icloud.com>
2021-04-17 22:55:46 -0500

    14. SPA 173. History push/replace

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,19 @@
 import React from "react";
 
-const LoginScreen = () => {
+const LoginScreen = ({ history }) => {
+  const handleClick = () => {
+    //? Diferencia entre psuh y replace...
+    //? replace no guarda la historia de navegacion, no permite ir ATRAS
+    //history.push("/");
+    history.replace("/");
+  };
   return (
-    <div>
+    <div className="container mt-5">
       <h1>LoginScreen</h1>
+      <hr />
+      <button className="btn btn-primary" onClick={handleClick}>
+        Login
+      </button>
     </div>
   );
 };
