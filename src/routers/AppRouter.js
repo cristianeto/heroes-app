5310fffdc4cce824c62152e08230b0e925c8606b
Cristian Guaman <cristian.guaman@icloud.com>
2021-04-17 22:47:45 -0500

    14. SPA 172. Creando un segundo Router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Navbar } from "../components/ui/Navbar";
 import LoginScreen from "../components/login/LoginScreen";
-import MarvelScreen from "../components/marvel/MarvelScreen";
+import DashboardRoutes from "./DashboardRoutes";
 
 const AppRouter = () => {
   return (
     <Router>
       <div>
-        <Navbar />
         <Switch>
           <Route exact path="/login" component={LoginScreen} />
-          <Route exact path="/" component={MarvelScreen} />
+          <Route path="/" component={DashboardRoutes} />
         </Switch>
       </div>
     </Router>
