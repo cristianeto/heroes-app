d1c15abdf4f362ef27ef7a032361bb9102cc696c
Cristian Guaman <cristian.guaman@icloud.com>
2021-04-22 20:06:14 -0500

    15. Proteccion de rutas 190. Logout del usuario

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,11 +1,20 @@
 import React, { useContext } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useHistory } from "react-router-dom";
 import { AuthContext } from "../../auth/AuthContext";
+import { types } from "../../types/types";
 
 export const Navbar = () => {
   const {
     user: { name },
+    dispatch,
   } = useContext(AuthContext);
+
+  const history = useHistory();
+
+  const handleLogout = () => {
+    history.replace("/login");
+    dispatch({ type: types.logout });
+  };
 
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark d-flex right">
@@ -46,14 +55,9 @@ export const Navbar = () => {
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
         <ul className="navbar-nav ml-auto">
           <span className="nav-item nav-link text-info">{name}</span>
-          <NavLink
-            activeClassName="active"
-            className="nav-item nav-link"
-            exact
-            to="/login"
-          >
+          <button className="nav-item nav-link btn" onClick={handleLogout}>
             Logout
-          </NavLink>
+          </button>
         </ul>
       </div>
     </nav>
