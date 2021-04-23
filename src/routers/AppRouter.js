fb0b88a23195b427e1ff93242cb6d00528ce72cd
Cristian Guaman <cristian.guaman@icloud.com>
2021-04-22 20:26:30 -0500

    15. Proteccion de rutas 191. Rutas privadas

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import LoginScreen from "../components/login/LoginScreen";
+
 import DashboardRoutes from "./DashboardRoutes";
+import LoginScreen from "../components/login/LoginScreen";
+import PrivateRoute from "./PrivateRoute";
+import { AuthContext } from "../auth/AuthContext";
 
 const AppRouter = () => {
+  const { user } = useContext(AuthContext);
+  console.log(user);
   return (
     <Router>
       <div>
         <Switch>
           <Route exact path="/login" component={LoginScreen} />
-          <Route path="/" component={DashboardRoutes} />
+
+          <PrivateRoute
+            path="/"
+            component={DashboardRoutes}
+            isAuthenticated={user.logged}
+          />
         </Switch>
       </div>
     </Router>
