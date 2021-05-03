1cb06360d205ba7e4925631481a4a95d515deb0d
Cristian Guaman <cristian.guaman@icloud.com>
2021-05-03 15:28:21 -0500

    Seccion 16. 206. Simular segmentos de URL en nuestras pruebas

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,14 +1,14 @@
-import React, { useMemo } from "react";
-import { useParams } from "react-router";
-import { getHeroById } from "../../selectors/getHeroById";
-import { Redirect } from "react-router-dom";
+import React, { useMemo } from 'react';
+import { useParams } from 'react-router';
+import { getHeroById } from '../../selectors/getHeroById';
+import { Redirect } from 'react-router-dom';
 
 const HeroScreen = ({ history }) => {
   const { heroId } = useParams();
   const hero = useMemo(() => getHeroById(heroId), [heroId]);
   //const hero = getHeroById(heroId);
-
-  if (!hero) return <Redirect to="/" />;
+  // console.log('hero:' + hero + '; heroId:' + heroId);
+  if (!hero) return <Redirect to='/' />;
 
   const {
     superhero,
@@ -20,40 +20,40 @@ const HeroScreen = ({ history }) => {
 
   const handleReturn = () => {
     if (history.length <= 2) {
-      history.push("/");
+      history.push('/');
     } else {
       history.goBack();
     }
   };
 
   return (
-    <div className="row mt-5">
-      <div className="col-4">
+    <div className='row mt-5'>
+      <div className='col-4'>
         <img
           alt={superhero}
-          className="img-thumbnail animate__animated animate__fadeInLeft"
+          className='img-thumbnail animate__animated animate__fadeInLeft'
           src={`../assets/heroes/${heroId}.jpg`}
         />
       </div>
-      <div className="col-8">
-        <h3 className="">{superhero}</h3>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
+      <div className='col-8'>
+        <h3 className=''>{superhero}</h3>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'>
             <b>Alter ego: </b>
             {alter_ego}
           </li>
-          <li className="list-group-item">
+          <li className='list-group-item'>
             <b>Publisher: </b>
             {publisher}
           </li>
-          <li className="list-group-item">
+          <li className='list-group-item'>
             <b>First appearance: </b>
             {first_appearance}
           </li>
         </ul>
         <h5>Characters</h5>
         <p>{characters}</p>
-        <button className="btn btn-outline-info" onClick={handleReturn}>
+        <button className='btn btn-outline-info' onClick={handleReturn}>
           Return
         </button>
       </div>
--- src/test/components/heroes/HeroScreen.test.js
+++ src/test/components/heroes/HeroScreen.test.js
@@ -1,6 +1,7 @@
 import { mount } from 'enzyme';
 import HeroScreen from '../../../components/heroes/HeroScreen';
 import { MemoryRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 describe('Testing in <HeroScreen />', () => {
   const historyMock = {
@@ -8,13 +9,72 @@ describe('Testing in <HeroScreen />', () => {
     push: jest.fn(),
     goBack: jest.fn(),
   };
-  const wrapper = mount(
-    <MemoryRouter initialEntries={['/hero']}>
-      <HeroScreen history={historyMock} />
-    </MemoryRouter>
-  );
 
   test('should show the component Redirect if there is not arguments at the URL', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/hero']}>
+        <HeroScreen history={historyMock} />
+      </MemoryRouter>
+    );
     expect(wrapper.find('Redirect').exists()).toBe(true);
+  });
+
+  test('should show a hero if the param in my URL exists', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/hero/marvel-spider']}>
+        <Route path='/hero/:heroId' component={HeroScreen} />
+      </MemoryRouter>
+    );
+    expect(wrapper.find('.row').exists()).toBe(true);
+  });
+
+  test('should return with history PUSH', () => {
+    const historyMock = {
+      length: 1,
+      push: jest.fn(),
+      goBack: jest.fn(),
+    };
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/hero/marvel-spider']}>
+        <Route
+          path='/hero/:heroId'
+          component={() => <HeroScreen history={historyMock} />}
+        />
+      </MemoryRouter>
+    );
+
+    wrapper.find('button').prop('onClick')();
+
+    expect(historyMock.push).toHaveBeenCalledWith('/');
+    expect(historyMock.goBack).not.toHaveBeenCalled();
+  });
+
+  test('should return back page with history goBack', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/hero/marvel-spider']}>
+        <Route
+          path='/hero/:heroId'
+          component={() => <HeroScreen history={historyMock} />}
+        />
+      </MemoryRouter>
+    );
+
+    wrapper.find('button').prop('onClick')();
+
+    expect(historyMock.push).toHaveBeenCalledTimes(0);
+    expect(historyMock.goBack).toHaveBeenCalled();
+  });
+
+  test('should return Redirect if hero doesnt exists', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/hero/marvel-spider123123']}>
+        <Route
+          path='/hero/:heroId'
+          component={() => <HeroScreen history={historyMock} />}
+        />
+      </MemoryRouter>
+    );
+
+    expect(wrapper.text()).toBe('');
   });
 });
