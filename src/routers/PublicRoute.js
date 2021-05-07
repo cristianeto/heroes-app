17292d36c270e8a0137fa1b3ab95332c5b829144
Cristian Guaman <cristian.guaman@icloud.com>
2021-05-07 09:38:15 -0500

    package updated

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -1,13 +1,13 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Redirect, Route } from "react-router-dom";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Redirect, Route } from 'react-router-dom';
 
 const PublicRoute = ({ isAuthenticated, component: Component, ...rest }) => {
   return (
     <Route
       {...rest}
       component={(props) =>
-        !isAuthenticated ? <Component {...props} /> : <Redirect to="/" />
+        !isAuthenticated ? <Component {...props} /> : <Redirect to='/' />
       }
     />
   );
